a3cf8c80231f69bd1499cabd511c068a30a5f997
JadyMcIntyre <116111920+JadyMcIntyre@users.noreply.github.com>
2024-06-01 15:15:45 +0200

    trying to fix the data sent

--- Controllers/MentorsController.cs
+++ Controllers/MentorsController.cs
@@ -1,21 +1,21 @@
 using godfidence.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Text.Json;
 namespace godfidence.Controllers;
-
 [Route("api/mentor")]
 [ApiController]
 public class MentorsController : ControllerBase
 {
-    private readonly Mentor[] _mentors =
-    [
-        new Mentor()
-            { Id = 1, Name = "Ivan", Expertise = "Rehabilitation", Description = "God saved me in x year from y." }
-    ];
+   private readonly Mentor[] _mentors =
+   [
+       new Mentor()
+           { Id = 1, Name = "Ivan", Expertise = "Rehabilitation", Description = "God saved me in x year from y." }
+   ];
 
-    [HttpGet]
-    public IEnumerable<Mentor> GetAllMentors()
-    {
-        return _mentors;
-    }
+   [HttpGet]
+   public IActionResult GetAllMentors()
+   {
+       var response = new { Mentors = _mentors }; // Encapsulate the mentors array in an object
+       return Ok(JsonSerializer.Serialize(response)); // Return the response object as a JSON string
+   }
 }
--- obj/Debug/net8.0/godfidence.AssemblyInfo.cs
+++ obj/Debug/net8.0/godfidence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("godfidence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ad376b7702c956559450e8c6ccb125f456bf02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("godfidence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("godfidence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
