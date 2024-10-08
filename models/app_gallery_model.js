5e8ce8645a981dc84086e46cb010cceae6a5b362
Jady McIntyre <116111920+JadyMcIntyre@users.noreply.github.com>
2024-10-08 19:27:50 +0200

    added comments to model

--- models/app_gallery_model.js
+++ models/app_gallery_model.js
@@ -1,12 +1,13 @@
-// mentorModel.js
+// Define a class representing an app
 class AppGallery {
     constructor(id, name) {
+        // Initialize the app with an ID and a name
         this.id = id;
         this.name = name;
     }
 }
 
-
+// Create an array of app instances representing the app gallery
 const apps = [
     new AppGallery(1, 'You Version'),
     new AppGallery(2, 'MessengerX'),
@@ -14,6 +15,7 @@ const apps = [
     new AppGallery(2, '412 Teens')
 ];
 
+// Export the AppGallery class and the apps array so they can be used in other parts of the application
 module.exports = {
     AppGallery,
     apps
