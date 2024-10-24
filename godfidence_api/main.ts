ddbdc27a379be521cbdafa71f0aa53f1bbf068a6
Jady McIntyre <116111920+JadyMcIntyre@users.noreply.github.com>
2024-10-24 11:15:20 +0200

    updated script, ran on port from env file
    changed dev script to allow all.
    ran basic index text on port

--- godfidence_api/main.ts
+++ godfidence_api/main.ts
@@ -1,3 +1,5 @@
 // Define the port for the server to listen on and for MongoDB
-const PORT = Number(Deno.env.get("PORT"));
+const API_PORT = Number(Deno.env.get("PORT"));
 const DB_PORT = Number(Deno.env.get("DB_PORT"));
+
+Deno.serve({port: API_PORT}, (req) => new Response("Hello, World!"));
