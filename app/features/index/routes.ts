58efc887481b1a6818ac69f3c07bf74b3652334f
Jady McIntyre <116111920+JadyMcIntyre@users.noreply.github.com>
2024-10-24 21:09:31 +0200

    Update routes.ts

--- app/features/index/routes.ts
+++ app/features/index/routes.ts
@@ -3,8 +3,8 @@ import { Router } from "https://deno.land/x/oak@v17.1.1/mod.ts";
 const router = new Router();
 
 export const indexRoutes = () => {
-    router.get("/index", (context) => {
-        context.response.body = { message: "Hello, World!" };
+    router.get("/", (context) => {
+        context.response.body = "Hello, World!";
     });
     return router;
 };
